1228b350d183d9db5d28e6957bde991c260485b3
4004h <minjihwan9500@gmail.com>
2024-08-07 12:30:38 +0900

    git

--- src/ch01/sec08/Hello.java
+++ src/ch01/sec08/Hello.java
@@ -4,6 +4,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Java08!!");
+		System.out.println("Hello Git08!!");
 	}
 }
